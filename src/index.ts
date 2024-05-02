6b466931ba9b1bceba72d787af6254558c78c256
KagChi <yonuxbot@gmail.com>
2024-05-02 16:58:35 +0700

    chore: testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export const main = (): string => "Hello, world! ";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { PGlite } from "@electric-sql/pglite";
+import { Server } from "hyper-express";
+
+const db = new PGlite("./db/pgdata");
+const server = new Server();
+
+type RequestBody = { sql: string; params: any[]; method: string; };
+
+server.post("/query", async (req, res) => {
+    const { sql, params, method } = await req.json<RequestBody, RequestBody>();
+
+    console.log(sql, params, method);
+
+    const sqlBody = sql.replaceAll(";", "");
+    try {
+        const result = await db.query(sqlBody, params, {
+            rowMode: method === "all" ? "array" : undefined
+        });
+        res.json(result.rows);
+    } catch (error: any) {
+        res.status(500).json({ error });
+    }
+    res.status(500).json({ error: "Unknown method value" });
+});
+
+await server.listen(3_001, "0.0.0.0");
+console.log("Server running at port 3001!");
